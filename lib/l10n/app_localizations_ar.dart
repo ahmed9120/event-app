e69bc252187deb4c4b4cbfdf0070cd8f1924cbea
Ahmed <aa929733@gmail.com>
2025-08-16 18:58:13 +0300

    finish firebase crud, login authentication with firebase auth

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,79 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get home => 'الرئيسيه';
+
+  @override
+  String get map => 'الخريطه';
+
+  @override
+  String get favorite => 'المفضله';
+
+  @override
+  String get profile => 'الملف الشخصي';
+
+  @override
+  String get language => 'اللغه';
+
+  @override
+  String get theme_mode => 'النمط';
+
+  @override
+  String get light => 'الفاتح';
+
+  @override
+  String get dark => 'الداكن';
+
+  @override
+  String get logout => 'تسجيل خروج';
+
+  @override
+  String get login => 'تسجيل دخول';
+
+  @override
+  String get register => 'تسجيل حساب';
+
+  @override
+  String get loginWithGoogle => 'تسجيل بحساب جوجل';
+
+  @override
+  String get email => 'الايميل';
+
+  @override
+  String get password => 'كلمه السر';
+
+  @override
+  String get repassword => 'اعاده كلمه السر';
+
+  @override
+  String get forgetPassword => 'نسيت كلمه السر';
+
+  @override
+  String get resetPassword => 'اعاده تعين كلمه السر';
+
+  @override
+  String get createAccount => 'أنشاء حساب';
+
+  @override
+  String get dontHaveAcc => 'ليس لديك حساب';
+
+  @override
+  String get alreadyHaveAcc => 'لديك حساب بالفعل';
+
+  @override
+  String get or => 'او';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get welcomeBack => 'مرحبا بعودتك ✨';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,79 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get map => 'Map';
+
+  @override
+  String get favorite => 'Favorite';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get theme_mode => 'Theme Mode';
+
+  @override
+  String get light => 'Light';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get loginWithGoogle => 'Login with Google';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get repassword => 'Re Password';
+
+  @override
+  String get forgetPassword => 'Forget Password';
+
+  @override
+  String get resetPassword => 'Reset Password';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get dontHaveAcc => 'Don\'t Have Account';
+
+  @override
+  String get alreadyHaveAcc => 'Already Have Account';
+
+  @override
+  String get or => 'or';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get welcomeBack => 'Welcome Back ✨';
+}
